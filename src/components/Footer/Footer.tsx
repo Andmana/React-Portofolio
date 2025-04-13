4f82311f75831c0b2161ac892a32e438b81d12ba
andmana <iam.andmana@gmail.com>
2025-04-13 21:44:11 +0700

    Build footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <footer className="fixed bottom-0 left-0 w-full h-14 flex justify-center items-center gap-[1ch]">
+            <span className="font-medium">©2025</span>
+            <a
+                className="font-bold underline underline-offset-4"
+                href="https://github.com/Andmana/React-Portofolio"
+            >
+                ANDMANA
+            </a>
+        </footer>
+    );
+};
+
+export default Footer;
