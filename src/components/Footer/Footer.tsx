a436dd44f3fff2f753e68f8af437067cbed62125
andmana <iam.andmana@gmail.com>
2025-04-14 15:20:10 +0700

    Add background inherit

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="fixed bottom-0 left-0 w-full h-14 flex justify-center items-center gap-[1ch]">
+        <footer className="fixed z-10 bottom-0 left-0 w-full h-14 bg-inherit flex justify-center items-center gap-[1ch]">
             <span className="font-medium">©2025</span>
             <a
                 className="font-bold underline underline-offset-4"
