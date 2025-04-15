2d068e937bb124f7e443453cec14a3ce4e01e0da
andmana <iam.andmana@gmail.com>
2025-04-15 18:47:39 +0700

    Add tags on about component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,6 +1,7 @@
-import { motion, useCycle } from "motion/react";
+import { color, motion, useCycle } from "motion/react";
 import SlideButton from "./SlideButton";
 import Header from "./Header";
+import { TAG } from "../../utils/Tags";
 
 const mainColor = "#FACC15";
 
@@ -11,20 +12,20 @@ const About = () => {
             style={{
                 minHeight: "calc(100vh - 112px)",
             }}
-            className="relative w-full flex  px-4 md:px-14 overflow-hidden"
+            className="relative w-full flex px-4 md:px-14 overflow-hidden"
             id="About"
         >
             <div className="relative flex-1 overflow-hidden">
                 <motion.div
-                    className="absolute top-0 left-0 w-[180%] h-full flex "
+                    className="absolute top-0 left-0 w-[200%] h-full flex portrait:flex-col "
                     initial={{ x: "0%" }}
-                    animate={{ x: showDetail ? "-40%" : "0%" }}
+                    animate={{ x: showDetail ? "-30%" : "0%" }}
                     transition={{ duration: 0.4, ease: "easeOut" }}
                 >
-                    <div className="relative h-full w-[56%] flex flex-col md:flex-row items-center">
+                    <div className="relative h-full w-[50%] portrait:h-fit portrait:w-full flex flex-col md:flex-row items-center">
                         <div
                             style={{ backgroundColor: mainColor }}
-                            className="w-3/11 h-full"
+                            className="w-3/11 h-full potrait:hidden"
                         />
 
                         <div className="absolute right-0 top-0 w-10/12 h-full flex items-center gap-4">
@@ -53,9 +54,9 @@ const About = () => {
                             />
                         </div>
                     </div>
-                    <div className="h-full w-[40%] p-4 md:p-14">
+                    <div className="h-full w-[50%] portrait:w-full p-8">
                         <article
-                            className="h-full w-full py-4 md:py-14 border-t-2 flex flex-col gap-3"
+                            className="h-full w-3/5 py-4 md:py-14 border-y-2 flex flex-col gap-3"
                             style={{ borderColor: mainColor }}
                         >
                             <h1 className="text-xl font-bold">
@@ -80,6 +81,22 @@ const About = () => {
                                 Here are the tech stacks I have used in various
                                 projects:
                             </p>
+                            <div className="mt-2 flex flex-wrap gap-2">
+                                {Object.entries(TAG).map(([key, tag]) => (
+                                    <div
+                                        className="p-1 text-[0.85rem] text-white font-semibold flex items-center gap-2"
+                                        style={{ backgroundColor: tag.color }}
+                                        key={key}
+                                    >
+                                        <img
+                                            src={tag.icon}
+                                            className="w-5 h-5"
+                                            alt=""
+                                        />
+                                        {tag.name}
+                                    </div>
+                                ))}
+                            </div>
                         </article>
                     </div>
                 </motion.div>
