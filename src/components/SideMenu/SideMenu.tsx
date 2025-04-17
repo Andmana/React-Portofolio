a9620b4613f7811291e3f81722f38693da7f8631
andmana <iam.andmana@gmail.com>
2025-04-17 21:05:17 +0700

    Apply slide on portrait device

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -6,18 +6,30 @@ import useSideMenu from "./UseSideMenu";
 import { useEffect } from "react";
 
 const SideMenu = () => {
-    const { ref } = useSideMenu();
+    const { ref, setIsDesktop, isDesktop } = useSideMenu();
     const { showMenu, setSideWidth, sideWidth } = useHomePage();
 
     useEffect(() => {
         const handleResize = () => {
             if (ref.current) {
-                setSideWidth(ref.current.offsetWidth);
+                //  Check position
+                const rect = ref.current.getBoundingClientRect();
+                const elementCenter = rect.left + rect.width / 2;
+                const viewportCenter = window.innerWidth / 2;
+
+                if (elementCenter < viewportCenter) {
+                    setSideWidth(ref.current.offsetWidth);
+                    setIsDesktop(true);
+                } else {
+                    setSideWidth(-ref.current.offsetWidth);
+                    setIsDesktop(false);
+                }
             }
         };
 
         window.addEventListener("resize", handleResize);
         handleResize();
+
         return () => {
             window.removeEventListener("resize", handleResize);
         };
@@ -25,14 +37,22 @@ const SideMenu = () => {
 
     return (
         <motion.aside
-            initial={{ x: "-100%" }}
-            animate={{ x: showMenu ? 0 : -sideWidth }}
+            initial={{ x: "0px" }}
+            animate={{
+                x: showMenu
+                    ? isDesktop
+                        ? 0
+                        : sideWidth
+                    : isDesktop
+                    ? -sideWidth
+                    : 0,
+            }}
             transition={{ duration: 0.4, ease: "easeOut" }}
             ref={ref}
-            className="fixed z-50 top-0 left-0 portrait:left-full w-3/5 lg:w-2/5 portrait:w-4/5 h-full portrait:h-svh flex px-4 lg:px-14 py-14 gap-4 lg:gap-14 items-stretch"
+            className="fixed z-30 top-0 landscape:left-0 portrait:left-full w-3/5 lg:w-2/5 portrait:w-4/5 h-full portrait:h-svh flex portrait:justify-end px-4 lg:px-14 py-14 gap-4 lg:gap-14 items-stretch"
         >
             {/* Hide Button */}
-            <div className="flex justify-center items-center portrait:hidden">
+            <div className="flex justify-center items-center portrait:hidden portrait:w-0">
                 <HideMenuButton />
             </div>
             {/* Menus */}
--- src/components/SideMenu/UseSideMenu.ts
+++ src/components/SideMenu/UseSideMenu.ts
@@ -1,9 +1,10 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const useSideMenu = () => {
     const ref = useRef<HTMLDivElement>(null);
+    const [isDesktop, setIsDesktop] = useState(false);
 
-    return { ref };
+    return { ref, isDesktop, setIsDesktop };
 };
 
 export default useSideMenu;
