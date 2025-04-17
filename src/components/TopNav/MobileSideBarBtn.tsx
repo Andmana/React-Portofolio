540bfc3bf352700f734104f33cc66d27180a5afd
andmana <iam.andmana@gmail.com>
2025-04-17 21:04:39 +0700

    Apply show/hide side menu on topnav button

--- src/components/TopNav/MobileSideBarBtn.tsx
+++ src/components/TopNav/MobileSideBarBtn.tsx
@@ -1,13 +1,19 @@
+import { motion } from "motion/react";
 import iconBurger from "../../assets/icons/icon-burger.svg";
+import iconCross from "../../assets/icons/icon-cross.svg";
+import { useHomePage } from "../../contexts/HomePageContext";
 
 const MobileSideBarButton = () => {
+    const { setShowMenu, showMenu } = useHomePage();
     return (
-        <button
+        <motion.button
+            whileHover={{ scale: 1.1 }}
+            onClick={() => setShowMenu((prev) => !prev)}
             className="w-7 h-7 dark:filter-(--whitening) cursor-pointer"
             aria-label="Sidebar"
         >
-            <img src={iconBurger} alt="" />
-        </button>
+            <img src={showMenu ? iconCross : iconBurger} alt="" />
+        </motion.button>
     );
 };
 
