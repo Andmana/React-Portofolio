259b29e1918fae010941ad747c7352ea31ca8f57
andmana <iam.andmana@gmail.com>
2025-04-17 15:25:33 +0700

    Change brightness according to theme

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -2,9 +2,11 @@ import { motion } from "motion/react";
 import Home from "../Home";
 import SideMenuButton from "./SideMenuButton";
 import { useHomePage } from "../../contexts/HomePageContext";
+import { useTheme } from "../../contexts/ThemeContext";
 
 const Content = () => {
     const { showMenu, sideWidth } = useHomePage();
+    const { darkMode } = useTheme();
 
     return (
         <motion.main className="relative w-full py-14 ">
@@ -13,7 +15,11 @@ const Content = () => {
                 initial={{ x: 0 }}
                 animate={{
                     x: showMenu ? sideWidth : 0,
-                    filter: showMenu ? "brightness(0.6)" : "brightness(1)",
+                    filter: showMenu
+                        ? darkMode
+                            ? "brightness(0.8)"
+                            : "brightness(1.4)"
+                        : "brightness(1)",
                 }}
                 transition={{ duration: 0.4, ease: "easeOut" }}
                 className="flex flex-col gap-14 "
