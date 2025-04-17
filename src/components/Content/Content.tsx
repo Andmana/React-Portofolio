5627ad00d2700c7a379e718fb63da69e2cf0c6c4
andmana <iam.andmana@gmail.com>
2025-04-17 12:13:44 +0700

    Create content component

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,13 @@
+import Home from "../Home";
+
+const Content = () => {
+    return (
+        <main className="relative w-full py-14 flex  flex-col gap-14">
+            <Home />
+            <section className="h-screen"></section>
+            <section className="h-screen"></section>
+        </main>
+    );
+};
+
+export default Content;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,4 @@
+import Content from "../../components/Content";
 import Footer from "../../components/Footer";
 import SideMenu from "../../components/SideMenu";
 import TopNav from "../../components/TopNav";
@@ -6,9 +7,11 @@ import { ThemeProvider } from "../../contexts/ThemeContext";
 const HomePage = () => {
     return (
         <ThemeProvider>
-            <div className="text-black dark:text-white bg-white dark:bg-[#242424]">
+            <div className="font-display text-black dark:text-white bg-white dark:bg-[#242424]">
                 <TopNav />
                 <SideMenu />
+
+                <Content />
                 <Footer />
             </div>
         </ThemeProvider>
