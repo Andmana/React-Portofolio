49559f63aea901a9bc3bdb9524955242b26253a3
andmana <iam.andmana@gmail.com>
2025-04-17 14:43:05 +0700

    Create side menu show button

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,8 +1,10 @@
 import Home from "../Home";
+import SideMenuButton from "./SideMenuButton";
 
 const Content = () => {
     return (
-        <main className="relative w-full py-14 flex  flex-col gap-14">
+        <main className="relative w-full py-14 flex flex-col gap-14">
+            <SideMenuButton />
             <Home />
             <section className="h-screen"></section>
             <section className="h-screen"></section>
---
+++ src/components/Content/SideMenuButton.tsx
@@ -0,0 +1,29 @@
+import { motion } from "motion/react";
+import iconBurger from "../../assets/icons/icon-burger.svg";
+import { useHomePage } from "../../contexts/HomePageContext";
+
+const SideMenuButton = () => {
+    const { showMenu, setShowMenu } = useHomePage();
+    return (
+        <motion.div
+            initial={{ x: "0px" }}
+            animate={{ x: showMenu ? "-200%" : "0%" }}
+            transition={{ duration: 0.4, ease: "easeOut" }}
+            className="fixed z-20 top-1/2 left-0 ps-14 transform -translate-y-1/2"
+        >
+            <button
+                onClick={() => setShowMenu(true)}
+                className="text-xl font-semibold flex flex-col items-center gap-4 rotate-90 animate-bounce "
+            >
+                <span className="transform rotate-180">MENU</span>
+                <img
+                    src={iconBurger}
+                    alt=""
+                    className="rotate-90 w-12 h-12 dark:filter-(--whitening)"
+                />
+            </button>
+        </motion.div>
+    );
+};
+
+export default SideMenuButton;
