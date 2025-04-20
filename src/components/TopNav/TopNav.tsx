ace32d536a7031314674e7edee26710a30ac0e2d
andmana <iam.andmana@gmail.com>
2025-04-20 18:43:03 +0700

    Use meqida query hook on top nav

--- src/components/TopNav/TopNav.tsx
+++ src/components/TopNav/TopNav.tsx
@@ -1,19 +1,25 @@
+import { useMediaQuery } from "react-responsive";
 import ThemeButton from "../ThemeButton";
 import MobileSideBarButton from "./MobileSideBarBtn";
 
 const TopNav = () => {
+    const isPotrait = useMediaQuery({ orientation: "portrait" });
+
     return (
-        <nav className="fixed z-50 top-0 left-0 w-full h-14 bg-inherit px-6 md:px-14 flex items-center">
+        <nav className="fixed z-50 top-0 left-0 w-full h-14 bg-inherit px-6 md:px-16 flex items-center">
             <a href="/" className="text-xl font-black ">
                 ANDMANA
             </a>
 
-            <div className="flex-1 flex items-center justify-end portrait:hidden">
-                <ThemeButton />
-            </div>
-            <div className="flex-1 flex items-center justify-end landscape:hidden">
-                <MobileSideBarButton />
-            </div>
+            {isPotrait ? (
+                <div className="flex-1 flex items-center justify-end">
+                    <MobileSideBarButton />
+                </div>
+            ) : (
+                <div className="flex-1 flex items-center justify-end">
+                    <ThemeButton />
+                </div>
+            )}
         </nav>
     );
 };
