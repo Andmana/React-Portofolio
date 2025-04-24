930acc7ef15402e83d1186e7185c51c1e453c329
andmana <iam.andmana@gmail.com>
2025-04-25 00:02:10 +0700

    Make responsive anchor element on side menu

--- src/components/SideMenu/Menu.tsx
+++ src/components/SideMenu/Menu.tsx
@@ -1,10 +1,13 @@
-import { motion } from "motion/react";
-import { useState } from "react";
 import SocialMedia from "./SocialMedia";
 import ThemeButton from "../ThemeButton";
 import { COLOR } from "../../constants/Color";
+import { useContent } from "../../contexts/ContentContext";
+import ColoredText from "../Common/ColoredText";
+import SectionAnchor from "./SectionAnchor";
 
 const Menu = () => {
+    const { activeContent } = useContent();
+
     return (
         <>
             <div className="not-portrait:hidden">
@@ -12,7 +15,8 @@ const Menu = () => {
             </div>
 
             <h1 className="text-4xl lg:text-6xl font-black">
-                <span>M</span>ENU
+                <ColoredText color={COLOR[activeContent]} label="M" />
+                ENU
             </h1>
 
             {/* Section */}
@@ -24,38 +28,16 @@ const Menu = () => {
 
             <div className="flex flex-col gap-4 text-lg portrait:text-base portrait:items-end font-light">
                 <h2 className="text-2xl lg:text-3xl font-bold">
-                    <span>C</span>ONTACT
+                    <ColoredText color={COLOR[activeContent]} label="C" />
+                    ONTACT
                 </h2>
-                <div className="text-left portrait:text-right portrait:text-sm">
+                <div className="text-left portrait:text-right portrait:text-sm font-medium">
                     <p className="">ANDREAS P. PUTRA S.</p>
                     <p>andre.pputras@gmail.com</p>
                 </div>
                 <SocialMedia />
             </div>
         </>
-    );
-};
-
-const SectionAnchor = ({ label }: { label: string }) => {
-    const [isHover, setIsHover] = useState(false);
-
-    return (
-        <motion.a
-            href="#"
-            onHoverStart={() => setIsHover(true)}
-            onHoverEnd={() => setIsHover(false)}
-            className="relative w-fit"
-        >
-            {label}
-            {/* hover effect */}
-            <motion.span
-                initial={{ scaleX: 0 }}
-                animate={{ scaleX: isHover ? 1 : 0 }}
-                transition={{ duration: 0.2, ease: "easeOut" }}
-                style={{ backgroundColor: COLOR[label] }}
-                className="absolute h-1/3 w-[120%] -left-1/10 top-1/2 transform -translate-y-2/5  origin-left"
-            ></motion.span>
-        </motion.a>
     );
 };
 
---
+++ src/components/SideMenu/SectionAnchor.tsx
@@ -0,0 +1,65 @@
+import { MouseEvent, useState } from "react";
+import { useHomePage } from "../../contexts/HomePageContext";
+import { useContent } from "../../contexts/ContentContext";
+import { motion } from "motion/react";
+import { COLOR } from "../../constants/Color";
+import ColoredText from "../Common/ColoredText";
+
+const SectionAnchor = ({ label }: { label: string }) => {
+    const [isHover, setIsHover] = useState(false);
+    const { setShowMenu } = useHomePage();
+    const { activeContent } = useContent();
+    const isActive = activeContent === label;
+
+    const clickHandle = (event: MouseEvent<HTMLAnchorElement>) => {
+        event.preventDefault();
+        setShowMenu(false);
+
+        setTimeout(() => {
+            // Scroll to #id label
+            const targetElement = document.getElementById(label);
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: "smooth", // Smooth scrolling
+                    block: "start", // Align to the top of the viewport
+                });
+            }
+        }, 500);
+    };
+
+    return (
+        <motion.a
+            onClick={clickHandle}
+            href={`#${label}`}
+            onHoverStart={() => setIsHover(true)}
+            onHoverEnd={() => setIsHover(false)}
+            className="relative w-fit"
+            style={{
+                color: isActive ? "gray" : "inherit",
+                cursor: isActive ? "none" : "pointer",
+                pointerEvents: isActive ? "none" : "auto",
+            }}
+            aria-label={`Go to section ${label}`} // Improve accessibility
+        >
+            <span>{label}</span>
+            {isActive && <ColoredText color={COLOR[activeContent]} label="." />}
+
+            {/* Hover effect */}
+            <motion.span
+                initial={{ scaleX: 0 }}
+                animate={{
+                    scaleX: !isActive ? (isHover ? 1 : 0) : 0,
+                    opacity: isHover || isActive ? 1 : 0, // Fade effect on hover
+                }}
+                transition={{ duration: 0.5, ease: "easeOut" }}
+                style={{
+                    backgroundColor: COLOR[label],
+                    opacity: 0, // Initial opacity
+                }}
+                className="absolute h-1/3 w-[120%] -left-1/10 top-1/2 transform -translate-y-2/5 origin-left"
+            ></motion.span>
+        </motion.a>
+    );
+};
+
+export default SectionAnchor;
