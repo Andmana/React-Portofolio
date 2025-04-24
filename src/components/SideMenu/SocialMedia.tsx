0c912e6310980bbb7e5261bbab6b90021591a34e
andmana <iam.andmana@gmail.com>
2025-04-25 00:02:49 +0700

    Change href link for social media

--- src/components/SideMenu/SocialMedia.tsx
+++ src/components/SideMenu/SocialMedia.tsx
@@ -1,31 +1,50 @@
-import { motion } from "motion/react";
+import { motion } from "framer-motion"; // Assuming you're using framer-motion
 import iconGh from "../../assets/icons/icon-github.svg";
 import iconIg from "../../assets/icons/icon-instagram.svg";
 import iconIn from "../../assets/icons/icon-linkedin.svg";
+import { MouseEvent } from "react";
 
 const contacts = [
-    { name: "Instagram", href: "/", icon: iconIg },
-    { name: "LinkedIn", href: "/", icon: iconIn },
-    { name: "Github", href: "/", icon: iconGh },
+    {
+        name: "Instagram",
+        href: "https://www.instagram.com/andmana.putra/",
+        icon: iconIg,
+    },
+    { name: "LinkedIn", href: "https://github.com/Andmana", icon: iconIn },
+    {
+        name: "Github",
+        href: "https://www.linkedin.com/in/andreas-permana-4a6b63241/",
+        icon: iconGh,
+    },
 ];
+
 const SocialMedia = () => {
+    const clickHandle = (event: MouseEvent<HTMLAnchorElement>) => {
+        event.preventDefault();
+        const target = event.target as HTMLAnchorElement;
+        window.open(target.href, "_blank");
+    };
+
     return (
         <div className="flex gap-2 portrait:justify-end">
             {contacts.map((media) => (
-                <a
+                <motion.a
                     key={media.name}
-                    className="w-14 h-14 portrait:w-10 portrait:h-10 "
+                    className="w-12 h-12 portrait:w-10 portrait:h-10"
                     href={media.href}
+                    onClick={clickHandle}
+                    aria-label={`Visit ${media.name}`}
+                    whileHover={{ scale: 1.1 }}
                 >
-                    <motion.img
+                    <img
                         src={media.icon}
-                        alt=""
-                        whileHover={{ scale: 1.1 }}
-                        className="dark:filter-(--whitening)"
+                        alt={`${media.name} Icon`}
+                        className="dark:filter-(--whitening) pointer-events-none"
                     />
-                </a>
+                </motion.a>
             ))}
         </div>
     );
 };
+
 export default SocialMedia;
