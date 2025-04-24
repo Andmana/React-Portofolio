dc70b7322d6032a6d50c6ef8dae6ecf5e5380c55
andmana <iam.andmana@gmail.com>
2025-04-24 22:31:12 +0700

    Create content context

---
+++ src/contexts/ContentContext.tsx
@@ -0,0 +1,38 @@
+import {
+    createContext,
+    Dispatch,
+    ReactNode,
+    SetStateAction,
+    useContext,
+    useEffect,
+    useState,
+} from "react";
+
+interface ContentContextType {
+    activeContent: string;
+    setActiveContent: Dispatch<SetStateAction<string>>;
+}
+
+const ContentContext = createContext<ContentContextType | undefined>(undefined);
+
+export const ContentProvider = ({ children }: { children: ReactNode }) => {
+    const [activeContent, setActiveContent] = useState("HOME");
+
+    useEffect(() => {
+        console.log("activeContent : ", activeContent);
+    }, [activeContent]);
+
+    return (
+        <ContentContext.Provider value={{ activeContent, setActiveContent }}>
+            {children}
+        </ContentContext.Provider>
+    );
+};
+
+export const useContent = () => {
+    const context = useContext(ContentContext);
+    if (!context) {
+        throw new Error("useContent must be used within a ThemeProvider");
+    }
+    return context;
+};
