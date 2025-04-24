335cfbe84fa9da93c1b432124168a4f8a9a5681f
andmana <iam.andmana@gmail.com>
2025-04-24 23:12:24 +0700

    Add hover effect based active content  color

--- src/components/ThemeButton/ThemeButton.tsx
+++ src/components/ThemeButton/ThemeButton.tsx
@@ -1,17 +1,22 @@
 import { motion } from "motion/react";
 import { useTheme } from "../../contexts/ThemeContext";
+import { useContent } from "../../contexts/ContentContext";
+import { COLOR } from "../../constants/Color";
 
 const ThemeButton = () => {
     const { darkMode, setDarkMode } = useTheme();
+    const { activeContent } = useContent();
 
     const clickHandler = () => {
         setDarkMode((prev) => !prev);
     };
 
     return (
-        <button
+        <motion.button
+            whileHover={{ backgroundColor: COLOR[activeContent] }}
+            style={{ backgroundColor: darkMode ? "#F1F5F9" : "#27272A" }}
             onClick={clickHandler}
-            className="w-13 h-5 bg-black dark:bg-white rounded-2xl flex items-center cursor-pointer"
+            className="w-13 h-5 rounded-2xl flex items-center cursor-pointer"
         >
             <motion.div
                 animate={{
@@ -25,7 +30,7 @@ const ThemeButton = () => {
                 transition={{ ease: "easeOut" }} // Optional: Add transition for smoothness
                 className="w-5 h-5 rounded-full bg-white dark:bg-black border-2 border-black dark:border-white box-content"
             ></motion.div>
-        </button>
+        </motion.button>
     );
 };
 
